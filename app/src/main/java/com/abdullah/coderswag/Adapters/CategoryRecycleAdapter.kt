207289b2d2e249dd4c867194e6ae3e35bc135399
Your Name <you@example.com>
2019-12-11 23:56:46 +0200

    refactored for recycler view adapter

---
+++ app/src/main/java/com/abdullah/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,40 @@
+package com.abdullah.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.abdullah.coderswag.Model.Category
+import com.abdullah.coderswag.R
+
+class CategoryRecycleAdapter(val contex : Context, val categories : List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val categoryImage = itemView.findViewById<ImageView>(R.id.categoryimage)
+        val categoryName = itemView.findViewById<TextView>(R.id.categoryName)
+
+        fun bindCategory(category : Category, contex : Context) {
+            val resourceId = contex.resources.getIdentifier(category.image, "drawable", contex.packageName)
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(contex).inflate(R.layout.category_list_item, parent, false)
+
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindCategory(categories[position], contex)
+    }
+}
--- app/src/main/java/com/abdullah/coderswag/Controler/MainActivity.kt
+++ app/src/main/java/com/abdullah/coderswag/Controler/MainActivity.kt
@@ -2,7 +2,9 @@ package com.abdullah.coderswag.Controler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.abdullah.coderswag.Adapters.CategoryAdapter
+import com.abdullah.coderswag.Adapters.CategoryRecycleAdapter
 import com.abdullah.coderswag.R
 import com.abdullah.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,13 +13,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        lateinit var adapter: CategoryAdapter
+        lateinit var adapter: CategoryRecycleAdapter
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.Categories)
+        adapter = CategoryRecycleAdapter(this, DataService.Categories)
         categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
